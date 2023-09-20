86e3d53fdb15da5c692ce7249539a038d3021c12
Palez8 <78558114+Palez8@users.noreply.github.com>
2023-09-20 15:47:57 +0200

    Add files via upload

---
+++ PalezWebsite/assets/scripts/localization.js
@@ -0,0 +1,37 @@
+var userLanguage = navigator.language || navigator.userLanguage;
+
+var translations = {
+    'en': {
+        'homepageKey': 'Homepage',
+        'testKey': 'Test',
+
+        'welcomeHomepageKey': 'Welcome to the Homepage',
+        'welcomeTestPageKey': 'Welcome to the Test Page',
+
+        'backToHomepageKey': 'Back to Homepage',
+    },
+    'de': {
+        'homepageKey': 'Startseite',
+        'testKey': 'Test',
+
+        'welcomeHomepageKey': 'Willkommen auf der Startseite',
+        'welcomeTestPageKey': 'Willkommen auf der Test Seite',
+
+        'backToHomepageKey': 'Zurueck zur Startseite',
+    },
+};
+
+function displayTranslation() {
+    var elements = document.querySelectorAll('[data-translation-key]');
+    
+    elements.forEach(function (element) {
+        var translationKey = element.getAttribute('data-translation-key');
+        var translation = translations[userLanguage] && translations[userLanguage][translationKey];
+        
+        if (translation) {
+            element.textContent = translation;
+        }
+    });
+}
+
+displayTranslation();
