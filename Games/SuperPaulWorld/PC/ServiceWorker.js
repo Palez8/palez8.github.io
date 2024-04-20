467af8bccea338faec16b2f261c5d27a9e6abe33
palez8 <pb@werdau.net>
2024-04-20 18:28:22 +0200

    Added SPW Web App

---
+++ Games/SuperPaulWorld/PC/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Palez-Super Paul World-1.2.6";
+const contentToCache = [
+    "Build/PC.loader.js",
+    "Build/PC.framework.js.unityweb",
+    "Build/PC.data.unityweb",
+    "Build/PC.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
